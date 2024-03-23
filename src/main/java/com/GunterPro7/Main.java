87b5e3490991198a9407a425c5fa4a102fd76ec3
GunterPro7 <florianrauscher700@gmail.com>
2024-03-23 16:58:55 +0100

    moved to server / client side application

--- src/main/java/com/GunterPro7/Main.java
+++ src/main/java/com/GunterPro7/Main.java
@@ -28,7 +28,6 @@ public class Main {
     public static boolean serverSide;
 
     public Main() {
-        new rdz();
         try {
             Minecraft.getInstance();
         } catch (RuntimeException e) {
